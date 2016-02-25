a8d255d8858941c5e2314ed44118d4a3737dabf2
RyanNovas <ryan@quill.org>
2016-02-24 23:03:54 -0500

    working ruby port

--- extension/content.js
+++ extension/content.js
@@ -21,28 +21,28 @@ fullTwitterShortenedLinks();
 
 
 
-// for (var i = 0; i < elements.length; i++) {
-//
-//   var element = elements[i];
-//   // var targets = ['bit.ly', 'ow.ly', 'is.gd', 'bitly.is'];
-//   if (element.innerHTML.indexOf('bit.ly') != -1 || element.innerHTML.indexOf('bitly.is') != -1 ||
-//     element.innerHTML.indexOf('ow.ly') != -1 ||
-//     element.innerHTML.indexOf('is.gd') != -1 ||
-//     element.innerHTML.indexOf('buff.ly') != -1)  {
-//   var pageContent = getContentByUrl(element.href, false);
-//   var matches = pageContent.match(/URL=(.*?)"/);
-//   var firstUrl = matches[1];
-//   var secondUrl = getContentByUrl("https://www.bitlonger.com/url.php?url=" + encodeURIComponent(firstUrl), false);
-//   if (secondUrl.indexOf('http') != -1) {
-//     element.innerHTML = secondUrl;
-//   }
-// }
-// }
-//
-// function getContentByUrl(url, synch_request) {
-//   var urlToGet = url.replace('http://', 'https://');
-//   var xmlHttp = new XMLHttpRequest();
-//   xmlHttp.open("GET", urlToGet, synch_request); // false for synchronous request
-//   xmlHttp.send(null);
-//   return xmlHttp.responseText;
-// }
+for (var i = 0; i < elements.length; i++) {
+
+  var element = elements[i];
+  if (element.innerHTML.indexOf('bit.ly') != -1 || element.innerHTML.indexOf('bitly.is') != -1 ||
+    element.innerHTML.indexOf('ow.ly') != -1 ||
+    element.innerHTML.indexOf('is.gd') != -1 ||
+    element.innerHTML.indexOf('buff.ly') != -1)  {
+  var pageContent = getContentByUrl(element.href, false, null);
+  var matches = pageContent.match(/URL=(.*?)"/);
+  var firstUrl = matches[1];
+  //var secondUrl = getContentByUrl("https://bitlonger.herokuapp.com?url=" + encodeURIComponent(firstUrl), false);
+  var secondUrl = getContentByUrl("http://localhost:4567", false, 'url=' + firstUrl);
+  if (secondUrl.indexOf('http') != -1) {
+    element.innerHTML = secondUrl;
+  }
+}
+}
+
+function getContentByUrl(url, synch_request, params) {
+  var urlToGet = url.replace('http://', 'https://');
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open("POST", urlToGet, synch_request); // false for synchronous request
+  xmlHttp.send(params);
+  return xmlHttp.responseText;
+}
